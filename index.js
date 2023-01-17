12b3e3f6e9f4ddc3a0e401954cb8a70340ece14b
koushiksarkerDIU <antusarker411@gmail.com>
2023-01-17 15:53:06 +0600

    mongodb setup & connected

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 const app = express();
 const cors = require('cors');
 app.use(cors());
+app.use(express.json())
 
 const Port = process.env.Port || 5000;
+
+
+const uri = "mongodb+srv://onlineFoodDelivery:z6xWzDANvuUJO3yJ@cluster0.z0daowb.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+async function run() {
+    try {
+        await client.connect();
+        console.log("database connected");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+run();
+
 
 app.listen(Port, () => {
     console.log("server is running", Port)
